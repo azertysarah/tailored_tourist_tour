b32491fa6dd7bafcf9fda0d4c22802381591269b
azertysarah <92745305+azertysarah@users.noreply.github.com>
2023-06-06 23:48:46 +0200

    clean service

--- backend2/src/main/java/com/example/backend2/controller/FormController.java
+++ backend2/src/main/java/com/example/backend2/controller/FormController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static java.lang.Math.round;
@@ -20,12 +21,11 @@ import static java.lang.Math.round;
 @CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 public class FormController {
-    private final MonumentRepository monumentRepository;
     private final MonumentService monumentService;
+    private static final double EARTH_RADIUS = 6371; // Radius of the Earth in kilometers
 
     @Autowired
-    public FormController(MonumentRepository monumentRepository, MonumentService monumentService) {
-        this.monumentRepository = monumentRepository;
+    public FormController(MonumentService monumentService) {
         this.monumentService = monumentService;
     }
     @PostMapping("/tours")
@@ -38,17 +38,16 @@ public class FormController {
         boolean needRealTime = data.isNeedRealTime();
 
         // Get the input monument from the database
-        Monument monument = this.monumentRepository.findByName(monumentName);
+        Monument monument = monumentService.getMonumentByName(monumentName);
 
         // Get all the monuments from the database
-        List<Monument> monuments = monumentRepository.findAll();
+        List<Monument> monuments = monumentService.getAllMonuments();
 
         // Filter the results if there is a requested period
         if(period != "") monuments = filterByPeriod(period, monuments);
 
         // Get the nearest monuments from the requested monument
-        List<Monument> nearestMonuments = this.monumentService.findNearestMonuments(monument, monuments);
-        // System.out.println(nearestMonuments.toString());
+        List<Monument> nearestMonuments = findNearestMonuments(monument, monuments);
 
         // Delete the ones that are not in the selected region
         //if(region != "") nearestMonuments = filterByRegion(region, nearestMonuments);
@@ -61,7 +60,7 @@ public class FormController {
     }
 
     public List<Monument> filterByPeriod(String period, List<Monument> monuments){
-        List<Monument> filteredMonuments = monumentRepository.findByPeriod(period);
+        List<Monument> filteredMonuments = monumentService.getMonumentByPeriod(period);
         for (Monument monument : monuments){
             if(monument.getFields().getSiecle() != null && monument.getFields().getSiecle().contains(period)) {
                 filteredMonuments.add(monument);
@@ -80,8 +79,57 @@ public class FormController {
             double pathTime = 5/currentMonument.getDistance(); // We suppose that the average speed of a walking person is equal to 5km/h
             timeLimit = timeLimit - round(pathTime)- 3; // We suppose that it takes 3 hours to visit a monument
             realTour.add(currentMonument);
-            monuments = monumentService.findNearestMonuments(currentMonument, monuments);
+            monuments = findNearestMonuments(currentMonument, monuments);
         }
         return realTour;
+    }
+
+    public List<Monument> findNearestMonuments(Monument monument, List<Monument> monuments) {
+        // Maximum distance is set to for 1 hour walking at the speed of 5km/h
+        double maximumDistance = 10;
+
+        // Get the coordinates of the monument
+        List<Double> coordinates = monument.getFields().getCoordinates();
+        Double monumentLatitude = coordinates.get(0);
+        Double monumentLongitude = coordinates.get(1);
+
+        // To prevent having monuments that has no coordinates
+        List<Monument> nearestMonuments = new ArrayList<>();
+
+        // Calculate distances and sort the monuments based on their distance to the inputted monument
+        monuments.forEach(item -> {
+            if(item.getFields().getCoordinates() != null) {
+                item.setDistance(
+                        calculateDistance(
+                                monumentLatitude,
+                                monumentLongitude,
+                                item.getFields().getCoordinates().get(0),
+                                item.getFields().getCoordinates().get(1)
+                        )
+                );
+                if(item.getDistance() <= maximumDistance) nearestMonuments.add(item);
+            }
+        });
+        // Sort list from most near monuments to most far
+        nearestMonuments.sort(Comparator.comparingDouble(Monument::getDistance));
+        return nearestMonuments;
+    }
+    private static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        // Convert the latitude and longitude from degrees to radians
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        // Calculate the differences in latitude and longitude in radians
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        // Calculate the central angle (c)
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // Calculate the distance using the Earth's radius (assuming a spherical Earth)
+        double distance = EARTH_RADIUS * c;
+
+        return distance;
     }
 }
--- backend2/src/main/java/com/example/backend2/service/MonumentService.java
+++ backend2/src/main/java/com/example/backend2/service/MonumentService.java
@@ -11,60 +11,23 @@ import java.util.List;
 @Service
 public class MonumentService {
     private final MonumentRepository monumentRepository;
-    private static final double EARTH_RADIUS = 6371; // Radius of the Earth in kilometers
+
 
     public MonumentService(MonumentRepository monumentRepository){
         this.monumentRepository = monumentRepository;
     }
 
-    public List<Monument> findNearestMonuments(Monument monument, List<Monument> monuments) {
-        // Maximum distance is set to for 1 hour walking at the speed of 5km/h
-        double maximumDistance = 10;
-
-        // Get the coordinates of the monument
-        List<Double> coordinates = monument.getFields().getCoordinates();
-        Double monumentLatitude = coordinates.get(0);
-        Double monumentLongitude = coordinates.get(1);
-
-        // To prevent having monuments that has no coordinates
-        List<Monument> nearestMonuments = new ArrayList<>();
-
-        // Calculate distances and sort the monuments based on their distance to the inputted monument
-        monuments.forEach(item -> {
-            if(item.getFields().getCoordinates() != null) {
-                item.setDistance(
-                        calculateDistance(
-                                monumentLatitude,
-                                monumentLongitude,
-                                item.getFields().getCoordinates().get(0),
-                                item.getFields().getCoordinates().get(1)
-                        )
-                );
-                if(item.getDistance() <= maximumDistance) nearestMonuments.add(item);
-            }
-        });
-        // Sort list from most near monuments to most far
-        nearestMonuments.sort(Comparator.comparingDouble(Monument::getDistance));
-        return nearestMonuments;
+    public Monument getMonumentByName(String monumentName) {
+        return this.monumentRepository.findByName(monumentName);
     }
 
-    private static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        // Convert the latitude and longitude from degrees to radians
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
-
-        // Calculate the differences in latitude and longitude in radians
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-        // Calculate the central angle (c)
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        // Calculate the distance using the Earth's radius (assuming a spherical Earth)
-        double distance = EARTH_RADIUS * c;
-
-        return distance;
+    public List<Monument> getAllMonuments() {
+        return monumentRepository.findAll();
     }
+
+    public List<Monument> getMonumentByPeriod(String period) {
+        return monumentRepository.findByPeriod(period);
+    }
+
 
 }
